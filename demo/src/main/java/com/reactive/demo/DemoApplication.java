e79f2b2f67d5699cdb18f7bfec8efcfdf50dffdf
EswaranMuthu <38020741+EswaranMuthu@users.noreply.github.com>
2018-04-05 20:03:58 -0400

    Add files via upload

---
+++ demo/src/main/java/com/reactive/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@org.springframework.scheduling.annotation.EnableAsync
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
